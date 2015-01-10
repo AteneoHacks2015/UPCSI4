e2fa371ab0f0c02adc06c30dcc827389d51e4515
Marc Francisco <marc.francisco.1124@gmail.com>
2015-01-10 18:05:37 +0800

    Applicants and joins migrate

--- project/db/migrate/20150110073758_create_sponsor_scholarship_joins.rb
+++ project/db/migrate/20150110073758_create_sponsor_scholarship_joins.rb
@@ -1,6 +1,8 @@
 class CreateSponsorScholarshipJoins < ActiveRecord::Migration
   def change
     create_table :sponsor_scholarship_joins do |t|
+    	t.integer :sp_id
+    	t.integer :sch_id
     end
   end
 end
