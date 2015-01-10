9d06b266af285306ceccdf334ca3372c06683a3f
Cjay <billonesciprian08@gmail.com>
2015-01-10 19:36:21 +0800

    Modified forms

--- project/app/controllers/applicants_controller.rb
+++ project/app/controllers/applicants_controller.rb
@@ -1,6 +1,26 @@
 class ApplicantsController < ApplicationController
 
 	def new
+		@name = params[:name]
+		@username = params[:username]
+		@email = params[:email]
+		@tag = params[:tag]
+		@account_id = (Accounts.last != nil) ? Accounts.last+1:1 
 	end
+
+	def create
+		@account = Accounts.new(accounts_params)
+
+		if @account.valid?
+			render plain: params[:accounts].inspect
+		else
+			redirect_to new_applicant_path(:tag=>0)
+		end
+	end
+
+	private
+		def accounts_params
+			params.require(:accounts).permit(:tag, :username, :email, :password, :password_confirmation)
+		end
 
 end
