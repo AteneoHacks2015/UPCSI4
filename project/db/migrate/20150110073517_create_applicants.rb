816f5c8a137922d7d6d714bbe2e750bc1cf883d3
Marc Francisco <marc.francisco.1124@gmail.com>
2015-01-10 19:01:48 +0800

    Editted Applicants and joins migrate

--- project/db/migrate/20150110073517_create_applicants.rb
+++ project/db/migrate/20150110073517_create_applicants.rb
@@ -1,6 +1,15 @@
 class CreateApplicants < ActiveRecord::Migration
   def change
     create_table :applicants do |t|
+    	t.string :name
+    	t.string :gender
+    	t.integer :add_id
+    	t.string :contact_num
+    	t.string :institution
+    	t.integer :age
+    	t.string :level
+    	t.string :degree_program
+    	t.string :email_add
     end
   end
 end
