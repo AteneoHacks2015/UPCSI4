baf34d04354cbb881fe64120b6bb52c812197afe
Cjay <billonesciprian08@gmail.com>
2015-01-11 00:35:08 +0800

    Merge profile and edited scholarship model

--- project/app/models/scholarship.rb
+++ project/app/models/scholarship.rb
@@ -1,2 +1,8 @@
 class Scholarship < ActiveRecord::Base
+	validates_presence_of :title
+	validates_presence_of :desc
+	validates_presence_of :slot
+	validates_numericality_of :slot
+	validates_presence_of :req
+	validates_presence_of :ben
 end
