13ef7bbba754fe933dc9a4b27a29984a5153e197
Cjay <billonesciprian08@gmail.com>
2015-01-10 20:29:47 +0800

    Added validations in models

--- project/app/controllers/sponsors_controller.rb
+++ project/app/controllers/sponsors_controller.rb
@@ -1,6 +1,26 @@
 class SponsorsController < ApplicationController
 
 	def new
+		@name = params[:name]
+		@username = params[:username]
+		@email = params[:email]
+		@tag = params[:tag]
+		@account_id = (Accounts.last != nil) ? Accounts.last+1:1 
 	end
+
+	def create
+		@account = Accounts.new(accounts_params)
+
+		if @account.valid?
+			render plain: params[:accounts].inspect
+		else
+			flash[:error] = @account.errors.full_messages.first
+		end
+	end
+
+	private
+		def accounts_params
+			params.require(:accounts).permit(:username, :email, :password, :password_confirmation)
+		end
 
 end
--- project/app/models/applicant.rb
+++ project/app/models/applicant.rb
@@ -1,2 +1,11 @@
 class Applicant < ActiveRecord::Base
+
+	validates_presence_of :name
+	validates_presence_of :gender
+	validates_presence_of :contact_num
+	validates_presence_of :institution
+	validates_presence_of :age
+	validates_numericality_of :age
+	validates_presence_of :level
+
 end
